08582206d6a7671f5604cb434b5956b51a4882ec
Hugo <hugo.ifalpalmeira@outlook.com>
2017-11-28 00:59:00 -0300

    Commiti inicial dos arquivos do projeto

---
+++ src/main/java/com/alagocode/cgip/api/CgipApplication.java
@@ -0,0 +1,12 @@
+package com.alagocode.cgip.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgipApplication.class, args);
+	}
+}
